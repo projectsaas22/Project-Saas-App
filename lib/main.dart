0552a8ec27a30867baec9118130b288988b25a69
Manish Acharya <manish.ce60@gmail.com>
2022-08-03 20:25:39 +0545

    feature: added start/stop button and export data to excel

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'dart:async';
+import 'dart:developer';
+import 'dart:io';
 
+import 'package:excel/excel.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:sensors_plus/sensors_plus.dart';
 
 void main() {
@@ -39,14 +46,24 @@ class _MyHomePageState extends State<MyHomePage> {
   List<double>? _gyroscopeValues;
   List<double>? _magnetometerValues;
   final _streamSubscriptions = <StreamSubscription<dynamic>>[];
+  final excel = Excel.createExcel();
+
+  static int rows = 2;
 
   @override
   Widget build(BuildContext context) {
+    var accelerometerX = "";
+    var accelerometerY = "";
+    var accelerometerZ = "";
+
     final accelerometer =
         _accelerometerValues?.map((double v) => v.toStringAsFixed(1)).toList();
-    final accelerometerX = _accelerometerValues![0].toStringAsFixed(5);
-    final accelerometerY = _accelerometerValues![1].toStringAsFixed(5);
-    final accelerometerZ = _accelerometerValues![2].toStringAsFixed(5);
+    if (_accelerometerValues != null) {
+      accelerometerX = _accelerometerValues![0].toStringAsFixed(5);
+      accelerometerY = _accelerometerValues![1].toStringAsFixed(5);
+      accelerometerZ = _accelerometerValues![2].toStringAsFixed(5);
+    }
+
     var gyroX = "";
     var gyroY = "";
     var gyroZ = "";
@@ -154,6 +171,61 @@ class _MyHomePageState extends State<MyHomePage> {
                           color: Color(0xFF424242),
                         ),
                   ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.green[100],
+                          shadowColor: Colors.transparent,
+                        ),
+                        onPressed: startSensor,
+                        child: Text(
+                          "Start | Record",
+                          style: Theme.of(context).textTheme.button!.copyWith(
+                                color: Colors.green[900],
+                                letterSpacing: 0.6,
+                                fontSize: 16,
+                              ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      ElevatedButton(
+                        onPressed: stopSensor,
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.red[100],
+                          shadowColor: Colors.transparent,
+                        ),
+                        child: Text(
+                          "Stop | Export",
+                          style: Theme.of(context).textTheme.button!.copyWith(
+                                color: Colors.red[900],
+                                letterSpacing: 0.6,
+                                fontSize: 16,
+                              ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Note: The file will be saved to your 'Downloads' folder.",
+                        style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                              fontWeight: FontWeight.w600,
+                            ),
+                      ),
+                    ],
+                  )
                 ],
               ),
             ),
@@ -171,15 +243,84 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  @override
-  void initState() {
-    super.initState();
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _streamSubscriptions.add(
+  //     accelerometerEvents.listen(
+  //       (AccelerometerEvent event) {
+  //         setState(() {
+  //           _accelerometerValues = <double>[event.x, event.y, event.z];
+  //         });
+  //       },
+  //     ),
+  //   );
+  //   _streamSubscriptions.add(
+  //     gyroscopeEvents.listen(
+  //       (GyroscopeEvent event) {
+  //         setState(() {
+  //           _gyroscopeValues = <double>[event.x, event.y, event.z];
+  //         });
+  //       },
+  //     ),
+  //   );
+  //   _streamSubscriptions.add(
+  //     userAccelerometerEvents.listen(
+  //       (UserAccelerometerEvent event) {
+  //         setState(() {
+  //           _userAccelerometerValues = <double>[event.x, event.y, event.z];
+  //         });
+  //       },
+  //     ),
+  //   );
+  //   _streamSubscriptions.add(
+  //     magnetometerEvents.listen(
+  //       (MagnetometerEvent event) {
+  //         setState(() {
+  //           _magnetometerValues = <double>[event.x, event.y, event.z];
+  //         });
+  //       },
+  //     ),
+  //   );
+  // }
+
+  void startSensor() {
+    final sheet = excel[excel.getDefaultSheet()!];
+    sheet.cell(CellIndex.indexByColumnRow(columnIndex: 1, rowIndex: 1)).value =
+        "Timestamp";
+
+    sheet.cell(CellIndex.indexByColumnRow(columnIndex: 2, rowIndex: 1)).value =
+        "Accelerometer-X";
+
+    sheet.cell(CellIndex.indexByColumnRow(columnIndex: 3, rowIndex: 1)).value =
+        "Accelerometer-Y";
+
+    sheet.cell(CellIndex.indexByColumnRow(columnIndex: 4, rowIndex: 1)).value =
+        "Accelerometer-Z";
+
     _streamSubscriptions.add(
       accelerometerEvents.listen(
         (AccelerometerEvent event) {
           setState(() {
             _accelerometerValues = <double>[event.x, event.y, event.z];
           });
+          print(DateFormat('kk:mm:ss:ms').format(DateTime.now()));
+          sheet
+              .cell(CellIndex.indexByColumnRow(columnIndex: 1, rowIndex: rows))
+              .value = DateFormat('kk:mm:ss:ms').format(DateTime.now());
+          sheet
+              .cell(CellIndex.indexByColumnRow(columnIndex: 2, rowIndex: rows))
+              .value = event.x;
+
+          sheet
+              .cell(CellIndex.indexByColumnRow(columnIndex: 3, rowIndex: rows))
+              .value = event.y;
+
+          sheet
+              .cell(CellIndex.indexByColumnRow(columnIndex: 4, rowIndex: rows))
+              .value = event.z;
+
+          rows++;
         },
       ),
     );
@@ -211,4 +352,45 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+
+  void stopSensor() async {
+    for (final subscription in _streamSubscriptions) {
+      subscription.cancel();
+    }
+    // var saveExcel = writeCounter(excel);
+    // print(saveExcel);
+
+    String filename =
+        "SensorTry" + DateFormat('kk-mm-ss').format(DateTime.now());
+    List<int>? excelFile = excel.save(fileName: 'SensorTry.xlsx');
+
+    PermissionStatus status = await Permission.storage.request();
+
+    if (status == PermissionStatus.granted) {
+      await File('/storage/emulated/0/Download/$filename.xlsx')
+          .writeAsBytes(excelFile!, flush: true)
+          .then((value) {
+        log('saved');
+      });
+    } else if (status == PermissionStatus.denied) {
+      log('Denied. Show a dialog with a reason and again ask for the permission.');
+    } else if (status == PermissionStatus.permanentlyDenied) {
+      log('Take the user to the settings page.');
+    }
+  }
+
+  // Future<String> get _localPath async {
+  //   final directory = await getApplicationDocumentsDirectory();
+  //   return directory.path;
+  // }
+
+  // Future<File> get _localFile async {
+  //   final path = await _localPath;
+  //   return File('$path/filename.xlsx');
+  // }
+
+  // Future<File> writeCounter(Excel excel) async {
+  //   final file = await _localFile;
+  //   return file.writeAsBytes(excel.encode()!);
+  // }
 }
